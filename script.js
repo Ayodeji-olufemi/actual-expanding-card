7ade0092cf56d1d1ef0f09962d8340c9e690d0d9
Ayodeji Akinyele <ayodejiakinyele@Ayodejis-MacBook-Pro.local>
2022-05-06 09:45:51 +0100

    expand

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasees()
+       panel.classList.add ('active')
+    })
+})
+
+function removeActiveClasees() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
